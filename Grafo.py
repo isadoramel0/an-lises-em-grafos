1256bfdefb647b2a7050c34ffde8657ae4ed90a2
Ana Clara Carvalho <ana.nascimento12@estudante.ufla.br>
2024-06-20 14:26:24 -0300

    feat: Verificação de possíveis erros de formatação do grafo e iniciando a modularização do projeto

--- Grafo.py
+++ Grafo.py
@@ -1,4 +1,25 @@
-print("Grafo.py")
+from Ler_MontarGrafo.montar import criar_grafo_de_arquivo
 
-# import Grafo 
+if __name__ == "__main__":
+    # pergunta ao usuário se o grafo é direcionado ou não;
+    while True:
+        resposta = input("O grafo é direcionado? (sim/não): ").strip().lower()
+        if resposta in ['sim', 's', 'Sim', 'SIM']:
+            direcionado = True
+            break
+        elif resposta in ['não', 'nao', 'n', 'Não', 'Nao', 'NAO', 'NÃO']:
+            direcionado = False
+            break
+        else:
+            print("Resposta inválida. Por favor, responda 'sim' ou 'não'.\n")
 
+    
+    filename = 'grafo.txt'
+    try:
+        # inicializa a criação e leitura do grafo;
+        grafo = criar_grafo_de_arquivo(filename, direcionado)
+        print(grafo) 
+    except ValueError as e:
+        print(f"Erro: {e}")  # erro no caso o formato seja inválido;
+    except FileNotFoundError as e:
+        print(f"Erro: Arquivo não encontrado - {e}")  # erro no caso de arquivo não encontrado;
